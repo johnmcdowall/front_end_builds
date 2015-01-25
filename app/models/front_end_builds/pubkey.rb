8d31b69eca455cca1102d1c8ef83b591816766c9
Ryan Toronto <ryanto@gmail.com>
2015-01-25 11:38:04 -0500

    start outlining test cases

--- app/models/front_end_builds/pubkey.rb
+++ app/models/front_end_builds/pubkey.rb
@@ -1,3 +1,7 @@
+require 'front_end_builds/utils/ssh_pubkey_convert'
+require 'base64'
+require 'openssl'
+
 module FrontEndBuilds
   class Pubkey < ActiveRecord::Base
     validates :name, presence: true
@@ -15,7 +19,26 @@ module FrontEndBuilds
       end
     end
 
-    def verify(signature, app, endpoint)
+    def ssh_pubkey?
+      (type, b64, _) = pubkey.split(/\s/)
+      %w{ssh-rsa ssh-dss}.include?(type) && b64.present?
+    end
+
+    # Public: In order to verify a signature we need the key to be an OpenSSL
+    # RSA PKey and not a string that you would find in an ssh pubkey key. Most
+    # people are going to be adding ssh public keys to their build system, this
+    # method will covert them to OpenSSL RSA if needed.
+    def fix_ssh_pubkey!
+      if ssh_pubkey?
+        key = FrontEndBuilds::Utils::SSHPubKeyConvert
+          .convert(pubkey)
+
+        self.pubkey = Base64.encode64(key)
+        save
+      end
+    end
+
+    def verify(signature, build)
 
     end
 
--- spec/models/front_end_builds/pubkey_spec.rb
+++ spec/models/front_end_builds/pubkey_spec.rb
@@ -4,8 +4,79 @@ module FrontEndBuilds
   RSpec.describe Pubkey, :type => :model do
     let(:pubkey) { FactoryGirl.create(:front_end_builds_pubkey) }
 
+    let(:rsa_private_key) do
+
+    end
+
+    let(:rsa_public_key) do
+
+    end
+
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:pubkey) }
+
+    describe '#fingerprint' do
+      it 'should give the finger print of the pubkey' do
+        pending
+
+      end
+
+      it 'should be unknown if it cannot figure out the pubkey' do
+        pending
+
+      end
+    end
+
+    describe '#ssh_pubkey?' do
+      it 'should be true for a ssh-rsa pubkey' do
+        pending
+
+      end
+
+      it 'should be true even if the key has no comment' do
+        pending
+
+      end
+
+      it 'should be false if the type of unknown' do
+        pending
+
+      end
+
+      it 'should be false if the key has no base64 encoded part' do
+        pending
+
+      end
+    end
+
+    describe '#fix_ssh_pubkey' do
+      it 'should fix an ssh pubkey' do
+        pending
+
+      end
+
+      it 'should not fix an open ssl rsa pkey' do
+        pending
+
+      end
+    end
+
+    describe '#verify' do
+      it 'should verify the signature is valid for a build' do
+        pending
+
+      end
+
+      it 'should not verify a bad signature for a build' do
+        pending
+
+      end
+
+      it 'should not be able to use a signature for multiple builds' do
+        pending
+
+      end
+    end
 
     describe '#serialize' do
       subject { pubkey.serialize }
